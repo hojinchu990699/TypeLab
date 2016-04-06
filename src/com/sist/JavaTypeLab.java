30e0968c3144f352da2754fa5665a055a451e492
hojin <chjin990@gmail.com>
2016-04-06 19:32:13 +0900

    자바 기본 데이터 타입 8가지 크기 출력

---
+++ src/com/sist/JavaTypeLab.java
@@ -0,0 +1,53 @@
+package com.sist;
+
+/**
+ * Created by user on 2016-04-05.
+ */
+public class JavaTypeLab {
+    public static void main(String[] args){
+
+        System.out.println("byte 범위 : " +Byte.MIN_VALUE+ "~" +Byte.MAX_VALUE);
+        System.out.println("short 범위 : " +Short.MIN_VALUE+ "~" + Short.MAX_VALUE);
+        System.out.println("int 범위 : " +Integer.MIN_VALUE+ "~" +Integer.MAX_VALUE);
+        System.out.println("long 범위 : " +Long.MIN_VALUE+ "~" +Long.MAX_VALUE);
+
+        System.out.println("float 범위 : "+Float.MIN_VALUE+ "~" +Float.MAX_VALUE);
+        System.out.println("double 범위 : "+Double.MIN_VALUE+ "~" +Double.MAX_VALUE);
+
+
+
+
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
